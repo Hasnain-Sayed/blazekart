9d5b8e4eafc9b5723e55a13663b44164df3f6e6b
Hasnain-Sayed <sayedmdhasnain05@gmail.com>
2026-02-27 16:38:02 +0530

    Fixed home random sample

--- blazekart/blazekart/views.py
+++ blazekart/blazekart/views.py
@@ -7,9 +7,12 @@ from django.db.models import Avg
 # Create your views here.
 def home(request):
     products=Product.objects.filter(stock__gt=0).annotate(avg_rating=Avg('reviews__rating'))
-    products=random.sample(list(products),k=4)
+    products = list(products)
+    sample_size = min(len(products), 4)
+
+    featured_products = random.sample(products, sample_size)
     context={
-        'all_prods':products,
+        'all_prods':featured_products,
     }
 
     return render(request,'index.html',context)
